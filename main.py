2ea521573231243bd713e0b11bedc2739faf9eaf
PotentialAviator15 <72152025+PotentialAviator15@users.noreply.github.com>
2022-02-28 19:21:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 import os
 from discord.ext import commands
-from discord.ext.commands import CommandNotFound, MissingRequiredArgument
+from discord.ext.commands import *
 
 client = commands.Bot(command_prefix="~")
 client.remove_command('help')
@@ -12,6 +12,8 @@ FounderAdminRole=836094933785903104
 regularAdminRole=836098165210873856
 developerRole=853327055394963516
 
+theServerIds = [836091554316681296]
+
 @client.event
 async def on_ready():
  await client.change_presence(status=discord.Status.idle, activity=discord.Game(name='discipleship'))
@@ -19,27 +21,27 @@ async def on_ready():
 
 
 
-@client.command()
+@client.slash_command(guild_ids=theServerIds)
 @commands.guild_only()
 @commands.has_any_role(moderator_role, FounderAdminRole, staff_role, regularAdminRole, developerRole)
 async def removerole(ctx, member : discord.Member, role : discord.Role):
     await member.remove_roles(role)
-    await ctx.send("**Role Removed!**")
+    await ctx.respond("**Role Removed!**", ephemeral=True)
 
-@client.command(aliases=['dl'])
+@client.command(guild_ids=theServerIds)
 @commands.has_permissions(manage_roles = True)
 @commands.guild_only()
 async def delrole(ctx, role: discord.Role):
-    await client.delete.role(role.server, role)
-    await client.say(f"The role {role.name} has been deleted!")
+    await ctx.delete.role(role.server, role)
+    await ctx.send(f"The role {role.name} has been deleted!", ephemeral=True)
   
-@client.command(aliases=['ub'])
+@client.slash_command(guild_ids=theServerIds)
 @commands.has_permissions(ban_members=True)
 @commands.guild_only()
 async def unban(ctx, id: int):
     user = await client.fetch.user(id)
     await ctx.guild.unban(user)
-    await ctx.send("**Successfully  unbanned!**")
+    await ctx.respond(f"**I have successfully  unbanned <@{id}>.**")
 
 @client.event
 async def on_command_error(ctx, error):
@@ -48,35 +50,34 @@ async def on_command_error(ctx, error):
     if isinstance(error, MissingRequiredArgument):
       await ctx.send("**You forgot to specify your command!**")
 
-@client.command(aliases=['b', 'excommuncate'])
+@client.slash_command(guild_ids=theServerIds)
 @commands.has_permissions(ban_members=True)
 @commands.guild_only()
 async def ban(ctx, user: discord.Member, *, reason=None):
   if user == None or user == ctx.message.author:
    return await ctx.channel.send("**You cannot ban yourself!** :rage:")
   await user.ban(reason=reason)
-  await ctx.send(f"**{user} has been bannned successfully!**", delete_after=5)
+  await ctx.respond(f"**{user} has been bannned successfully!**", ephemeral=True)
   await ctx.message.delete()
 
-@client.command(aliases=['gr'])
-@commands.guild_only()
-@commands.has_permissions(manage_roles = True)
+@client.slash_command(guild_ids=theServerIds)
+@commands.has_permissions(manage_roles=True)
 async def giverole(ctx, member : discord.Member, role : discord.Role):
     await member.add_roles(role)
-    await ctx.send("**Role Added!**")
+    await ctx.respond(f"**I have given {member.display_name} {role.name}.**")
 
 #Misc
 
-@client.command(aliases=['av'])
-async def avatar(ctx, *,  avamember : discord.Member=None):
-    userAvatarUrl = avamember.avatar.url
+@client.slash_command(guild_ids=theServerIds)
+async def avatar(ctx, *,  user : discord.Member=None):
+    userAvatarUrl = user.avatar.url
     embedav = discord.Embed(title="Here is their avatar:", Description="lol", color=0xffffff)
     embedav.set_.image(url=f'{userAvatarUrl}')
-    await ctx.send(embed=embedav)
+    await ctx.respond(embed=embedav)
 
 #library
 
-@client.command()
+@client.command(aliases=['lib'])
 async def library(ctx):
   embedlib = discord.Embed(title="Library", description="This is currently being worked on. Thanks in advance for your patience!")
   embedlib.add_field(name="~trinity", value="***In Development***", inline=False)
@@ -90,13 +91,12 @@ async def trinity(ctx):
   embedtri.set_thumbnail(url="https://cdn.discordapp.com/avatars/908030197889126481/1d3436efc1bcc8472e54c46f2c10ce64.png?size=1024")
   await ctx.send(embed=embedtri) 
 
-@client.command()
-@commands.guild_only()
+@client.slash_command(guild_ids=theServerIds)
 async def staff(ctx): 
   embedstaff = discord.Embed(title="Our Staff Team", description="We work hard to share the Gospel and develop a good relationship with our members. If you would like to dm any one of us, feel free to do so! \n\n\n **Admins:** \n\n <@!221495055452471297> \n <@!751556186548994149> \n\n\n **Moderators:** \n\n <@!854173257288777800> \n<@!904780290780717149> \n<@!707530980831002644> \n<@!572560639373869057> \n<@!384309507087663104> \n<@!187552171229839360> \n\n Thanks so much for being a part of this server!")
   embedstaff.set_thumbnail(url="https://cdn.discordapp.com/avatars/908030197889126481/1d3436efc1bcc8472e54c46f2c10ce64.png?size=1024")
   embedstaff.set_footer(text="Hope for the Lost ~ Christian Apologetics", icon_url="https://cdn.discordapp.com/avatars/908030197889126481/1d3436efc1bcc8472e54c46f2c10ce64.png?size=1024")
-  await ctx.send(embed=embedstaff)
+  await ctx.respond(embed=embedstaff)
 
 @client.command()
 async def owner(ctx):
@@ -108,9 +108,9 @@ async def jesus(ctx):
   embedjesu.set_footer(text="Hope for the Lost ~ Christian Apologetics", icon_url="https://cdn.discordapp.com/avatars/908030197889126481/1d3436efc1bcc8472e54c46f2c10ce64.png?size=1024")
   await ctx.send(embed=embedjesu)
 
-@client.command()
-async def salvation(ctx):
-  embedsal = discord.Embed(title="", description="")
+# @client.command()
+# async def salvation(ctx):
+#   embedsal = discord.Embed(title="", description="")
 
 
 token = os.environ.get("TOKEN")
